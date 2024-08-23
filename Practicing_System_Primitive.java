14b0c17f4d0d195da88a64a757d628e623a57c39
ReidenYonesaki <reiden.yonesaki@gmail.com>
2024-08-23 13:22:17 -1000

    Create Practicing_System_Primitive.java

---
+++ Practicing_System_Primitive.java
@@ -0,0 +1,12 @@
+public class MyClass {
+  public static void main(String args[]) {
+  
+    System.out.println("OOOOOOOOO  OOOOOOOOOOO OOOOO OOOOOOOOO  OOOOOOOOOOO OOOO   OOOO");
+    System.out.println("OOO    OOO  OOO    OO   OOO   OOO    OOO OOO    OO   OOOOO  OO ");
+    System.out.println("OOOOOOOOO   OOOOOOO     OOO   OOO    OOO OOOOOOO     OO OOOOOO ");
+    System.out.println("OOO  OOO    OOO    OO   OOO   OOO    OOO OOO    OO   OO   OOOO");
+    System.out.println("OOOO  OOOO  OOOOOOOOO  OOOOO OOOOOOOOO  OOOOOOOOOOO OOOO    OO ");
+
+    
+  }
+}
