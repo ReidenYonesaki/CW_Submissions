5959bdae216ab2dd801a4433dd99ef0200167606
ReidenYonesaki <reiden.yonesaki@gmail.com>
2024-08-28 19:56:54 -1000

    Update Practicing_System_Primitive.java

--- Practicing_System_Primitive.java
+++ Practicing_System_Primitive.java
@@ -7,6 +7,25 @@ public class MyClass {
     System.out.println("OOO  OOO    OOO    OO   OOO   OOO    OOO OOO    OO   OO   OOOO");
     System.out.println("OOOO  OOOO  OOOOOOOOO  OOOOO OOOOOOOOO  OOOOOOOOOOO OOOO    OO ");
 
-    
+    double prod1=6.0*3.5;
+			double prod2=1.5*5;
+			double sub=prod1-prod2;
+			double quo=55.6-30.2;
+			double answ=sub/quo;
+			
+			System.out.print(answ);
+			
+			System.out.println("");
+			System.out.println("");
+
+      double pi=3.1519;
+			double rad=4.0;
+			double rad2=16.0;
+				
+			System.out.print("The circumference of the circle is ");
+			System.out.print(2*pi*rad);
+			System.out.print(" and the area of the circle is ");
+			System.out.print(pi*rad2);
+			System.out.print(".");
   }
 }
