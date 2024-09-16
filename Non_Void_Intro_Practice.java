f8923629f22971818a5de5a583e067908ef0a906
ReidenYonesaki <reiden.yonesaki@gmail.com>
2024-09-15 19:21:12 -1000

    Create Non_Void_Intro_Practice.java

---
+++ Non_Void_Intro_Practice.java
@@ -0,0 +1,33 @@
+public class Non_Void_Method {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(stateName());
+		System.out.println(stateEyeColor());
+		System.out.println(stateFavoriteFood());
+		System.out.println(stateHairStyle());
+		System.out.println(stateBestFriend());
+		
+	}
+	public static String stateName()
+	{
+		return("My name is Reiden Yonesaki");
+	
+	}
+	public static String stateEyeColor()
+	{
+		return("My eye color is brown") ;
+	}
+	public static String stateHairStyle()
+	{
+		return ("My hairstyle is a side comb") ;
+	}
+	public static String stateBestFriend()
+	{
+		return("My best friend is Justin Nguyen");
+	}
+	public static String stateFavoriteFood()
+	{
+		return("My favorite food is Loco Moco") ;
+	 }
+	
