9818e15c6b992723f92e60914f8cfff2977234a6
ReidenYonesaki <reiden.yonesaki@gmail.com>
2025-01-28 09:56:54 -1000

    Create Arrays_Intro.java

---
+++ Arrays_Intro.java
@@ -0,0 +1,66 @@
+
+public class Arrays_Intro {
+
+
+	public static void main(String[] args) {
+
+    
+		double [] doubArr;
+		
+		int [] intArr;
+		
+		boolean [] booleanArr;
+		
+		String [] stringArr;
+	
+		Circle [] circleArr;
+
+
+
+    
+
+
+		
+		int[] values= {7, -12, 88, -88, 0, 5,}
+
+      
+		
+		String[] fruitsArr=new String[]
+      
+
+
+
+      
+		fruitsArr[1]="Bapple";
+
+
+    
+
+
+		
+		fruitsArr[fruitsArr.length-1]="Zanana";
+
+
+    
+	
+
+		System.out.println(fruitsArr[4]);
+
+
+    
+		 
+	
+		System.out.println(fruitsArr[1]);
+	
+		
+		
+
+		
+		for(int i=0; i<fruitsArr.length;i++)
+	
+		{
+			System.out.println(fruitsArr[i]);
+		}
+		
+	}
+}
